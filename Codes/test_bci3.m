bf6f2cc246a6be2b7848f925f1f53403f6fabfe1
mohammadraziei <mohammadraziei1375@gmail.com>
2020-02-11 16:13:13 +0330

    .75 to .9 two layer svm

--- Codes/test_bci3.m
+++ Codes/test_bci3.m
@@ -25,15 +25,15 @@ load('regenerated_data2.mat')
 % [P_thinking,W_thinking] = pwelchTrial(EEG_thinking_diff);
 % [P_talking,W_talking] = pwelchTrial(EEG_talking_diff);
 
-% [P_noTask, W_noTask] = pwelch(EEG_noTask);
-% [P_listening,W_listening] = pwelchTrial(EEG_listening);
-% [P_thinking,W_thinking] = pwelchTrial(EEG_thinking);
-% [P_talking,W_talking] = pwelchTrial(EEG_talking);
+[P_noTask, W_noTask] = pwelch(EEG_noTask);
+[P_listening,W_listening] = pwelchTrial(EEG_listening);
+[P_thinking,W_thinking] = pwelchTrial(EEG_thinking);
+[P_talking,W_talking] = pwelchTrial(EEG_talking);
 
-P_noTask = fft(EEG_noTask,[],1); W_noTask = linspace(0,pi,size(P_noTask,1));
-P_listening = fft(EEG_listening,[],1);
-P_thinking = fft(EEG_thinking,[],1);
-P_talking = fft(EEG_talking,[],1);
+% P_noTask = fft(EEG_noTask,[],1); W_noTask = linspace(0,pi,size(P_noTask,1));
+% P_listening = fft(EEG_listening,[],1);
+% P_thinking = fft(EEG_thinking,[],1);
+% P_talking = fft(EEG_talking,[],1);
 
 %%
 resamp = @(x,tx,a) resample(x,tx,a/(tx(2)-tx(1)),'linear',1,1); % 'linear', 'spline', 'pchip'
@@ -42,16 +42,16 @@ listening = P_listening;
 thinking = P_thinking;
 talking = P_talking;
 
-% [ref,~] = resamp(P_noTask,W_noTask,0.0157);% 0.0157 ,0.01825
-[ref,~] = resamp(P_noTask,W_noTask,0.01825);% 0.0157 ,0.01825
+[ref,~] = resamp(P_noTask,W_noTask,0.0157);% 0.0157 ,0.01825
+% [ref,~] = resamp(P_noTask,W_noTask,0.01825);% 0.0157 ,0.01825
 ref = smooth2D(ref);
 ref = repmat(ref,1,1,20);
 listening = listening ./ ref;
 thinking = thinking ./ ref;
 talking = talking ./ ref;
 
-F_cutoff = 41;
-F_cuton = 4;
+F_cutoff = 45;
+F_cuton = 5;
 Narr = size(P_listening,1);
 cutoff = floor(F_cutoff * 2 / fs * Narr);
 cuton = ceil(F_cuton * 2 / fs * Narr);
@@ -117,7 +117,7 @@ reshapeTrial = @(a) reshape(permute(a,[2,1,3]),size(a,1)*size(a,2),size(a,3))';
 
 Nfit = 5;
 fitures = [myFeatureSelection(thinking,Nfit)];%; myFeatureSelection(listening,Nfit); myFeatureSelection(talking,Nfit)];
-fitures = [(abs(fitures)); (angle(fitures))];
+% fitures = [(abs(fitures)); (angle(fitures))];
 fitures = reshapeTrial(fitures);
 
 %%
@@ -131,12 +131,13 @@ for i = 1:20
     pred(i) = (label == test_Y);
     beta(:,i) = mdl.Beta;
     [~,idx] = sort(mdl.Beta);
-    channel(:,i) = idx;%ceil(idx/(Nfit*3));
+    channel(:,i) = idx;%ceil(idx/(Nfit*2));
 end
 
 sum(pred)/20
 B = mean(beta,2);
 [B,idx]=sort(B);
+channel2 = ceil(channel/(Nfit*1));
 %%
 
 % chan1 = 51;chan2 = 34;
@@ -151,4 +152,35 @@ B = mean(beta,2);
 
 % mdl = fitglm(X./max(X),order_label, 'Distribution','binomial');
 % score_log = mdl.Fitted.Probability; % Probability estimates
+
+%%
+channels = unique(channel2(1:50,:));
+
+fitures = [myFeatureSelection(thinking,Nfit)];
+fitures = fitures(:,channels,:);
+fitures = [(abs(fitures)); (angle(fitures))];
+fitures = reshapeTrial(fitures);
+
+
+pred = zeros(20,1);
+for i = 1:20
+    train_X = fitures; train_Y = order_label;
+    train_X(i,:) = []; train_Y(i,:) = [];
+    test_X = fitures(i,:); test_Y = order_label(i,:);
+    mdl=fitcsvm(train_X, train_Y,'Standardize',true);
+    [label,score] = predict(mdl,test_X);
+    pred(i) = (label == test_Y);
+    beta2(:,i) = mdl.Beta;
+    [~,idx] = sort(mdl.Beta);
+    channel3(:,i) = idx;%ceil(idx/(Nfit*2));
+end
+
+sum(pred)/20
+B = mean(beta2,2);
+[B,idx]=sort(B);
+channel4 = ceil(channel3/(Nfit*1));
+
+
+
+
 
